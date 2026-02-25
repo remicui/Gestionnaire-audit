0f32fba4ad0332210e9f14580c3c3b36144dc613
remicui <remi.culliere@bechameil.com>
2026-02-25 10:39:28 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'bechameil-v1';
+const ASSETS = [
+  'index.html',
+  'Logo_.png'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Stratégie : Répondre avec le cache si hors ligne
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
