e7e065f178719c23c8d3e0ab99181a0b64581180
remicui <remi.culliere@bechameil.com>
2026-08-14 09:18:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,36 @@
-const CACHE_NAME = 'bechameil-v1';
-const ASSETS = [
-  'index.html',
-  'Logo_.png'
+const CACHE_NAME = 'bechameil-audit-v1';
+const FILES_TO_CACHE = [
+    './index-mobile.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
-// Installation : Mise en cache des fichiers
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
 });
 
-// Stratégie : Répondre avec le cache si hors ligne
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    // Réseau d'abord (pour avoir les dernières mises à jour), secours sur le cache si hors-ligne
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const copy = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
 });
